30ca92847fd32f39f4f3314535a17882af76a8ba
OfWanderer <117962823+OfWanderer@users.noreply.github.com>
2023-01-30 21:52:50 +0300

    Initiate "Plan" - Laying out the plan which will be commented out later on the project.

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("Hello World.");
+console.log("This is Cap'n Kirk of the Enterprise.");
+console.log("Let's go over the \"Plan\" before we start the Project: Rock Paper Scissors");
+console.log("For future of the planet Earth may depend on it.");
+
+let plan = `
+• Does your program have a user interface ?
+
+    - We do not need a UI (User Interface) for current version of the game.
+    
+• What inputs will your program have ?
+
+    - There will be options to be chosen among which is either "Rock", "Paper" or "Scissors".
+
+    - These options will be "randomly" chosen by the computer and "specifically" by the person who is playing the game.
+
+    - "Rock" beats "Scissors", "Scissors cuts the paper." and lastly "Paper wraps the rock.".
+
+• What is the desired output ?
+
+    - The desired output is to announce the winner of the game wether it is the computer or the person who is playing.
+
+• Given your inputs, what are the necessary steps to return the desired output ?
+    -
+    `
+
+console.log(plan);
